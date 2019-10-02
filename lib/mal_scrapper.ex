8fd146332dbd92b6aee5516550d05e7c38f9226a
Chris Fuertes <cristhian.fuertes@correounivalle.edu.co>
2019-10-01 21:29:00 -0500

    Adding details to MalScrapper.top/2's result

--- lib/mal_scrapper.ex
+++ lib/mal_scrapper.ex
@@ -5,10 +5,10 @@ defmodule MalScrapper do
    @user_agent [{"User-agent", "MAL Wrapper"}]
 
    def top(type, page \\ 1) when page >= 1 do
-       suffix = if page == 1, do: "", else: "?limit = #{(page - 1) * 50}"
-       "https://myanimelist.net/topanime.php?type=#{type}#{suffix}"
-         |> HTTPoison.get( @user_agent)
-         |> handle_response()
+     suffix = if page == 1, do: "", else: "?limit=#{(page - 1) * 50}"
+     "https://myanimelist.net/topanime.php?type=#{type}#{suffix}"
+       |> HTTPoison.get( @user_agent)
+       |> handle_response()
    end
 
 
@@ -29,19 +29,36 @@ defmodule MalScrapper do
   end
 
   defp parse_data (data) do
-   data |> Floki.find(".hoverinfo_trigger ")  
-        |> Floki.text(sep: "+")
-        |> String.split("+")
-        |> Enum.with_index()
-   # extra = data |> Floki.find(".detail, .information, .di-ib, .mt4")
-   #               |> Floki.text(sep: "+") 
-   #               |> String.split("+")
-   #               |> Enum.slice(8, 50)
+   details = data 
+             |> Floki.find("div.information, .di-ib mt4")
+             |> get_anime_details()
 
-   #|> Enum.map(&String.trim(&1))
-   # ranks =  data |> Floki.find(".lightLink, .top-anime-rank-text")
-   #            |> Floki.text(sep: "+") 
-   #            |> String.split("+")
-   # names
+   names = data 
+              |> Floki.find(".hoverinfo_trigger ")  
+              |> Floki.text(sep: "+")
+              |> String.split("+")
+
+   Enum.with_index(names) 
+   |> Enum.zip(details) 
+   |> Enum.map(fn {{name, index}, details} -> 
+      %{name: name, rank: index + 1, details: details} 
+    end)
+  end
+
+  defp get_anime_details(items) do
+    for item <- items do
+      with {"div", _, raw_details} = item do
+         get_details(raw_details)
+      end 
+    end
+  end
+
+  defp get_details(raw_details) do
+    raw_details |> Enum.reduce("", fn (d, acc) -> 
+     case d do
+        {"br", _, _} -> acc
+        txt -> acc <> "|" <> String.trim(txt)
+      end
+     end)
   end
 end
